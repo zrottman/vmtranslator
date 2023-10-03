58f5344f910aae00c2c7b63864977e19a827879e
zrottman <zrottman@example.com>
2023-10-03 09:22:41 -0700

    Add initial tests for lookup

--- test/test_lookup.c
+++ test/test_lookup.c
@@ -12,6 +12,13 @@ void tearDown(void)
 void test_get_command_type(void)
 {
     TEST_ASSERT_EQUAL(C_ARITHMETIC, get_command_type("add"));
+    TEST_ASSERT_EQUAL(C_PUSH, get_command_type("push"));
+    TEST_ASSERT_EQUAL(C_IF, get_command_type("if-goto"));
+}
+
+void test_get_command_for_invalid_strings(void)
+{
+    TEST_ASSERT_EQUAL(C_UNKNOWN, get_command_type("fake command"));
 }
 
 int main(void)
@@ -19,6 +26,8 @@ int main(void)
     UNITY_BEGIN();
 
     RUN_TEST(test_get_command_type);
+    RUN_TEST(test_get_command_for_invalid_strings);
+
 
     return UNITY_END();
 }
