ece05e2f40f10c4b0bcb02b2a7f2f1cfb5601169
zrottman <zrottman@example.com>
2023-10-04 16:20:44 -0700

    WIP: need to finish implementing write_arithmetic()

--- src/writer.c
+++ src/writer.c
@@ -48,65 +48,118 @@ int write_comment(const char* line, FILE* fp) {
 
 int write_pushpop(enum Command command, enum Segment segment, char* idx, FILE* fp) {
     switch (segment) {
+
         case S_LOCAL:
         case S_ARGUMENT:
         case S_THIS:
         case S_THAT:
-            if (command == C_PUSH) {
-                fprintf(fp, "@%s\n", idx);
-                fputs("D=A\n", fp);
-                fprintf(fp, "%s\n", (lookup_seg_type(segment)));
-                fputs("A=D+M\n", fp);
-                fputs("D+M\n", fp);
-                fputs(PUSH, fp);
-            } else if (command == C_POP) {
-                fprintf(fp, "@%s\n", idx);
-                fputs("D=A\n", fp);
-                fprintf(fp, "%s\n", (lookup_seg_type(segment)));
-                fputs("D=D+M\n", fp);
-                fputs(POP, fp);
+            fprintf(fp, "@%s\n", idx);
+            fputs("D=A\n", fp);
+            fprintf(fp, "%s\n", (lookup_seg_type(segment)));
+            switch (command) {
+                case C_PUSH:
+                    fputs("A=D+M\n", fp);
+                    fputs("D+M\n", fp);
+                    fputs(PUSH, fp);
+                    break;
+                case C_POP:
+                    fputs("D=D+M\n", fp);
+                    fputs(POP, fp);
+                    break;
             }
             break;
+
         case S_POINTER:
         case S_TEMP:
-            if (command == C_PUSH) {
-                fprintf(fp, "@%s\n", idx);
-                fputs("D=A\n", fp);
-                fprintf(fp, "%s\n", (lookup_seg_type(segment)));
-                fputs("A=D+A\n", fp);
-                fputs("D+M\n", fp);
-                fputs(PUSH, fp);
-            } else if (command == C_POP) {
-                fprintf(fp, "@%s\n", idx);
-                fputs("D=A\n", fp);
-                fprintf(fp, "%s\n", (lookup_seg_type(segment)));
-                fputs("D=D+A\n", fp);
-                fputs(POP, fp);
+            fprintf(fp, "@%s\n", idx);
+            fputs("D=A\n", fp);
+            fprintf(fp, "%s\n", (lookup_seg_type(segment)));
+            switch (command) {
+                case C_PUSH:
+                    fputs("A=D+A\n", fp);
+                    fputs("D+M\n", fp);
+                    fputs(PUSH, fp);
+                    break;
+                case C_POP:
+                    fputs("D=D+A\n", fp);
+                    fputs(POP, fp);
+                    break;
             }
             break;
+
         case S_STATIC:
-            if (command == C_PUSH) {
-                fprintf(fp, "%s\n", (lookup_seg_type(segment)));
-                fputs("D=M\n", fp);
-                fputs(PUSH, fp);
-            } else if (command == C_POP) {
-                fprintf(fp, "%s\n", (lookup_seg_type(segment)));
-                fputs("D=A\n", fp);
-                fputs(POP, fp);
+            fprintf(fp, "%s\n", (lookup_seg_type(segment)));
+            switch (command) {
+                case C_PUSH:
+                    fputs("D=M\n", fp);
+                    fputs(PUSH, fp);
+                    break;
+                case C_POP:
+                    fputs("D=A\n", fp);
+                    fputs(POP, fp);
+                    break;
             }
             break;
+
         case S_CONSTANT:
-            if (command == C_PUSH) {
-                fprintf(fp, "@%s\n", idx);
-                fputs("D=A\n", fp);
-                fputs(PUSH, fp);
-            } else if (command == C_POP) {
+            if (command != C_PUSH) {
                 // error
             }
+            fprintf(fp, "@%s\n", idx);
+            fputs("D=A\n", fp);
+            fputs(PUSH, fp);
             break;
+
         case S_UNKNOWN:
             // error
             break;
+    }
+    return 0;
+}
+
+int write_arithmetic(enum Command command, FILE* fp) {
+    switch (command) {
+
+        case NEG:
+        case NOT:
+            fputs(UNARY_LOAD, fp);
+            switch (command) {
+                case NEG:
+                    fputs("M=-M\n", fp);
+                    break;
+                case NOT:
+                    fputs("M=!M\n", fp);
+                    break;
+            }
+            fputs(INC, fp);
+            break;
+
+        case ADD;
+        case SUB;
+        case AND;
+        case OR;
+            fputs(BINARY_LOAD, fp);
+            switch (command) {
+                case ADD:
+                    fputs("M=D+M\n");
+                    break;
+                case SUB:
+                    fputs("M=D-M\n");
+                    break;
+                case AND:
+                    fputs("M=D&M\n");
+                    break;
+                case OR:
+                    fputs("M=D|M\n");
+                    break;
+            }
+            fputs(INC, fp);
+            break;
+        case EQ;
+        case GT;
+        case LT;
+            break;
+            
     }
     return 0;
 }
